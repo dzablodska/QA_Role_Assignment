f8c8dbe9723fc73ed747d72a34859881a589b30c
dzablodska <daria.zablodska@gmail.com>
2018-07-29 13:36:36 +0200

    Add assertions

--- cypress/integration/Tests_QA_Role_Assigment/simple_spec.js
+++ cypress/integration/Tests_QA_Role_Assigment/simple_spec.js
@@ -1,14 +1,12 @@
 describe('My First Test', function() {
     it('Visits the github', function() {
-        cy.clearLocalStorage()
+        //cy.clearLocalStorage()
         // Login in github with your account
         cy.visit('https://github.com/login')
         cy.get('#login_field')
             .type('daria.zablodska@gmail.com')
-            .should('have.value', 'daria.zablodska@gmail.com')
         cy.get('#password')
             .type('Test1234')
-            .should('have.value', 'Test1234')
         cy.get('input[type="submit"]').click()
         cy.get('#user-links img.avatar').should('have.attr', 'alt', '@dzablodska')
 
@@ -16,10 +14,15 @@ describe('My First Test', function() {
         cy.get('.btn.btn-sm.btn-primary.text-white').click()
         cy.get('#repository_name')
             .type('Test_repo')
-            .should('have.value', 'Test_repo')
+
         //Create README.md file in repository and commit&push it via Github UI
         cy.get('#repository_auto_init').click()
         cy.get('.btn.btn-primary.first-in-line').click()
+        cy.url().should('eq', 'https://github.com/dzablodska/Test_repo')
+        cy.get('table.files').contains('README.md')
+            .should('have.attr', 'href', '/dzablodska/Test_repo/blob/master/README.md')
+        cy.get('ul.numbers-summary').contains('commit')
+            .should('have.attr', 'href', '/dzablodska/Test_repo/commits/master')
 
         //Delete repository
         cy.get('a[href="/dzablodska/Test_repo/settings"]').click()
@@ -27,6 +30,9 @@ describe('My First Test', function() {
         cy.get('input[name="verify"]:visible').click()
         .type('Test_repo')
         cy.get('form[action="/dzablodska/Test_repo/settings/delete"] button.btn-danger[type="submit"][data-disable-invalid]').click()
+        cy.get('ul[data-filterable-type="substring"]').contains('Test_repo')
+            .should('not.have.attr', 'href', '/dzablodska/Test_repo')
+
 
 
     })
